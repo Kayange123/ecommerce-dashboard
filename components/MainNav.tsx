5804f24c4a7b23c72936bbe7bdc8584568ea9578
Ayubu Kayange <kayangejr3@gmail.com>
2023-09-15 20:01:46 +0300

    mobile hambuger menu implementation

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,59 +1,126 @@
 "use client";
-import { cn } from '@/lib/utils'
-import Link from 'next/link';
-import { useParams, usePathname } from 'next/navigation'
-import React from 'react'
-
-const MainNav = ({className, ...props}: React.HTMLAttributes<HTMLElement>) => {
-    const pathname = usePathname();
-    const params = useParams();
-    const routes = [
-        {
-            href: `/${params.storeId}`,
-            label: 'Overview',
-            active: pathname === `/${params.storeId}`,
-        },
-        {
-            href: `/${params.storeId}/billboards`,
-            label: 'My billboards',
-            active: pathname === `/${params.storeId}/billboards`,
-        },
-        {
-            href: `/${params.storeId}/categories`,
-            label: 'Categories',
-            active: pathname === `/${params.storeId}/categories`,
-        },
-        {
-            href: `/${params.storeId}/products`,
-            label: 'Products',
-            active: pathname === `/${params.storeId}/products`,
-        },
-        {
-            href: `/${params.storeId}/sizes`,
-            label: 'My sizes',
-            active: pathname === `/${params.storeId}/sizes`,
-        },
-        {
-            href: `/${params.storeId}/orders`,
-            label: 'My orders',
-            active: pathname === `/${params.storeId}/orders`,
-        },
-        {
-            href: `/${params.storeId}/settings`,
-            label: 'Settings',
-            active: pathname === `/${params.storeId}/settings`,
-        }
-    ];
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import { Menu, X } from "lucide-react";
+import { Dialog } from "@headlessui/react";
+import { UserButton } from "@clerk/nextjs";
+import IconButton from "./ui/IconButton";
+const MainNav = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) => {
+  const pathname = usePathname();
+  const params = useParams();
+  const [isOpen, setIsOpen] = useState(false);
+  const onOpen = () => setIsOpen(true);
+  const onClose = () => setIsOpen(false);
+  const routes = [
+    {
+      href: `/${params.storeId}`,
+      label: "Overview",
+      active: pathname === `/${params.storeId}`,
+    },
+    {
+      href: `/${params.storeId}/billboards`,
+      label: "My billboards",
+      active: pathname === `/${params.storeId}/billboards`,
+    },
+    {
+      href: `/${params.storeId}/categories`,
+      label: "Categories",
+      active: pathname === `/${params.storeId}/categories`,
+    },
+    {
+      href: `/${params.storeId}/products`,
+      label: "Products",
+      active: pathname === `/${params.storeId}/products`,
+    },
+    {
+      href: `/${params.storeId}/sizes`,
+      label: "My sizes",
+      active: pathname === `/${params.storeId}/sizes`,
+    },
+    {
+      href: `/${params.storeId}/orders`,
+      label: "My orders",
+      active: pathname === `/${params.storeId}/orders`,
+    },
+    {
+      href: `/${params.storeId}/settings`,
+      label: "Settings",
+      active: pathname === `/${params.storeId}/settings`,
+    },
+  ];
   return (
-    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
-        {routes.map(route=>(
-            <Link key={route.href} href={route.href} 
-            className={cn("text-sm font-medium transition-colors hover:text-primary", route.active ? "text-black dark:text-white" : "text-muted-foreground")}>
-                {route.label}
+    <>
+      <nav className={cn("space-x-4", className)}>
+        <div
+          className={cn("hidden lg:flex items-center space-x-4 lg:space-x-6")}
+        >
+          {routes.map((route) => (
+            <Link
+              key={route.href}
+              href={route.href}
+              className={cn(
+                "text-sm font-medium transition-colors hover:text-primary",
+                route.active
+                  ? "text-black dark:text-white"
+                  : "text-muted-foreground"
+              )}
+            >
+              {route.label}
             </Link>
-        ))}
-    </nav>
-  )
-}
+          ))}
+          <div className="mx-auto flex justify-center items-center space-x-4">
+            <UserButton afterSignOutUrl="/" />
+          </div>
+        </div>
+        <div className="lg:hidden">
+          <Button className="lg:hidden bg-white" onClick={onOpen}>
+            <Menu size={25} color="black" />
+          </Button>
+          <Dialog
+            onClose={onClose}
+            open={isOpen}
+            as="div"
+            className="relative z-40 lg:hidden"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+            <div className="flex fixed inset-0">
+              <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto shadow-xl bg-white py-6 pb-4">
+                <div className="flex items-center justify-end px-4">
+                  <IconButton icon={<X size={20} />} onClick={onClose} />
+                </div>
+                <div className="mx-auto flex justify-center items-center space-x-4">
+                  <UserButton afterSignOutUrl="/" />
+                </div>
+                <div className="flex flex-col w-full space-y-3 mt-10 items-center">
+                  {routes.map((route) => (
+                    <Link
+                      onClick={onClose}
+                      key={route.href}
+                      href={route.href}
+                      className={cn(
+                        " font-medium text-xl transition-colors hover:text-primary",
+                        route.active
+                          ? "text-black dark:text-white"
+                          : "text-muted-foreground"
+                      )}
+                    >
+                      {route.label}
+                    </Link>
+                  ))}
+                </div>
+              </Dialog.Panel>
+            </div>
+          </Dialog>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-export default MainNav
+export default MainNav;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,31 +1,28 @@
-import { UserButton, auth } from '@clerk/nextjs'
-import React from 'react'
-import MainNav from './MainNav'
-import StoreSwitcher from '@/components/store-switcher'
-import { redirect } from 'next/navigation'
-import prismadb from '@/lib/prismadb'
+import { UserButton, auth } from "@clerk/nextjs";
+import React from "react";
+import MainNav from "./MainNav";
+import StoreSwitcher from "@/components/store-switcher";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
 
 const Navbar = async () => {
-    const {userId} =  auth();
-    if(!userId){
-        redirect('/sign-in');
-    }
-    const stores = await prismadb.store.findMany({
-        where : {
-            userId  
-        }
-    })
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/sign-in");
+  }
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
   return (
-    <div className='border-b'>
-        <div className="flex h-14 items-center px-4">
-            <StoreSwitcher items={stores}/>
-            <MainNav className='mx-6' />
-            <div className='ml-auto flex items-center space-x-4'>
-                <UserButton afterSignOutUrl='/' />
-            </div>
-        </div>
+    <div className="border-b py-2">
+      <div className="flex h-14 items-center justify-between px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className=" " />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
