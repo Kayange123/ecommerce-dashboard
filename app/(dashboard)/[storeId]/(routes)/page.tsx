23b50b93ea568fe7cf05dcabd7bd3d8d9519dd29
Ayubu Kayange <kayangejr3@gmail.com>
2023-09-15 20:09:17 +0300

    new features added

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,18 +1,78 @@
-import prismadb from "@/lib/prismadb"
+import {
+  getGraphRevenue,
+  getStockSize,
+  getTotalRevenue,
+  getTotalSales,
+} from "@/actions/actions";
+import Overview from "@/components/Overview";
+import Heading from "@/components/ui/Heading";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { priceFormat } from "@/lib/utils";
+import { CreditCard, DollarSign, Package } from "lucide-react";
 
 interface DashboardPageProps {
-    params: {storeId: string}
+  params: { storeId: string };
 }
 
-const DashboardPage = async ({params}: DashboardPageProps) => {
-    const store = await prismadb.store.findFirst({
-        where :{
-            userId: params?.storeId
-        }
-    })
+const DashboardPage = async ({ params }: DashboardPageProps) => {
+  const revenue = await getTotalRevenue(params.storeId);
+  const salesCount = await getTotalSales(params.storeId);
+  const stockCount = await getStockSize(params.storeId);
+  const graphData = await getGraphRevenue(params.storeId);
   return (
-    <div>DashboardPage</div>
-  )
-}
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description="Overview of your store" />
+        <Separator />
+        <div className="grid gap-4 grid-cols-3">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Total revenue
+              </CardTitle>
+              <DollarSign className="text-muted-foreground w-4 h-4" />
+            </CardHeader>
+            <CardContent>
+              <p className="text-sm lg:text-2xl font-bold">
+                {priceFormat.format(revenue)}
+              </p>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Number of sales
+              </CardTitle>
+              <CreditCard className="text-muted-foreground w-4 h-4" />
+            </CardHeader>
+            <CardContent>
+              <p className="text-sm lg:text-2xl font-bold">{salesCount}</p>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Products In stock
+              </CardTitle>
+              <Package className="text-muted-foreground w-4 h-4" />
+            </CardHeader>
+            <CardContent>
+              <p className="text-sm lg:text-2xl font-bold">{stockCount}</p>
+            </CardContent>
+          </Card>
+        </div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Overview</CardTitle>
+          </CardHeader>
+          <CardContent className="pl-2">
+            {/* <Overview data={graphData} /> */}
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,22 +1,17 @@
-'use client'
-
+"use client";
 
 import { Modal } from "@/components/ui/modal";
 import { useStoreModal } from "@/hooks/useStoreModal";
 import { useEffect } from "react";
 
 export default function SetupPage() {
-  const {isOpen, onOpen} = useStoreModal();
+  const { isOpen, onOpen } = useStoreModal();
 
-  useEffect(()=>{
-    if(!isOpen){
+  useEffect(() => {
+    if (!isOpen) {
       onOpen();
     }
-  },[isOpen, onOpen])
-  
-  return (
-    <div className="p-4">
-      
-    </div>
-  )
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4"></div>;
 }
